932962b6410c46df55458290554d5a154fa90fd1
Beatriz Ocampo <bitandehuitocampo@outlook.com>
2025-04-19 17:55:18 -0600

    Commit: test para resta (fallo)

--- tests/test_operaciones.py
+++ tests/test_operaciones.py
@@ -1,6 +1,7 @@
 #test/test_operaciones.py
 import pytest 
 from calculadora.operaciones import suma
+from calculadora.operaciones import resta
 
 def test_suma():
     assert suma(2, 3) == 5
@@ -8,3 +9,7 @@ def test_suma():
     assert suma(0, 0) == 0
     assert suma(2.5, 3.5) == 6.0
 
+def test_resta():
+    assert resta(5, 3) == 2
+    assert resta(3,5) == -2
+    assert resta(2.5, 1.5) == 1.0
