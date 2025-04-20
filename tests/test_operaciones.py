3f0b79211ae817ff019a5f5ab364f402cd44cf31
Beatriz Ocampo <bitandehuitocampo@outlook.com>
2025-04-19 18:06:30 -0600

    Commit: test multiplicación (falló)

--- tests/test_operaciones.py
+++ tests/test_operaciones.py
@@ -2,6 +2,12 @@
 import pytest 
 from calculadora.operaciones import suma
 from calculadora.operaciones import resta
+from calculadora.operaciones import multiplicacion
+
+def test_multiplicacion():
+    assert multiplicacion(3, 4) == 12
+    assert multiplicacion(-2, 3) == -6
+    assert multiplicacion(2.5, 4) == 10.0
 
 def test_suma():
     assert suma(2, 3) == 5
@@ -13,3 +19,4 @@ def test_resta():
     assert resta(5, 3) == 2
     assert resta(3,5) == -2
     assert resta(2.5, 1.5) == 1.0
+
